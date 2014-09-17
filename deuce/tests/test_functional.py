33ea866d5fe70f19f0b1740e0bb1fe10cc2e7a1c
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2014-09-17 09:23:39 -0400

    rm: auth token header
    Shouldnt be required, since by default the openstack hook
    is disabled.

--- deuce/tests/test_functional.py
+++ deuce/tests/test_functional.py
@@ -6,7 +6,6 @@ from deuce.tests import V1Base
 class TestRootController(V1Base):
 
     def test_get(self):
-        # Require project ID for root as well
         response = self.simulate_get('/', headers={})
         self.assertEqual(self.srmock.status, falcon.HTTP_404)
 
@@ -14,10 +13,9 @@ class TestRootController(V1Base):
                                               self.create_project_id()})
         self.assertEqual(self.srmock.status, falcon.HTTP_404)
 
-    def test_get_10(self):
+    def test_get_v1(self):
         response = self.simulate_get('/v1.0', headers={'x-project-id':
-                                                  self.create_project_id(),
-            'x-auth-token': self.create_auth_token()})
+                                                  self.create_project_id()})
         self.assertEqual(self.srmock.status, falcon.HTTP_200)
 
     def test_get_not_found(self):
