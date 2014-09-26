b1315f7c5517626d7717e8a136f1d9d650c10658
Jamie Painter <jamie.painter@rackspace.com>
2014-09-26 10:57:50 -0400

    Incremental change

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
 # -*- coding: utf-8 -*-
 import sys
-try:
-    from distutils.core import setup
-    from setuptools import find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
-else:
-    REQUIRES = ['configobj', 'falcon', 'six', 'setuptools >= 1.1.6',
-                'cassandra-driver', 'pymongo', 'msgpack-python',
-                'python-swiftclient', 'python-logstash', 'asyncio',
-                'aiohttp']
-    setup(
-        name='deuce',
-        version='0.2',
-        description='Deuce - Block-level de-duplication as-a-service',
-        license='Apache License 2.0',
-        url='github.com/rackerlabs/deuce',
-        author='Rackspace',
-        author_email='',
-        install_requires=REQUIRES,
-        test_suite='deuce',
-        zip_safe=False,
-        entry_points={
-            'console_scripts': [
-                'deuce-server = deuce.cmd.server:run',
-            ]
-        },
-        data_files=[('config', ['ini/config.ini', 'ini/configspec.ini'])],
-        include_package_data=True,
-        packages=find_packages(exclude=['tests*', 'deuce/tests*']),
-    )
+
+from setuptools import setup, find_packages
+
+REQUIRES = ['configobj', 'falcon', 'six', 'setuptools >= 1.1.6',
+            'cassandra-driver', 'pymongo', 'msgpack-python',
+            'python-swiftclient', 'python-logstash', 'asyncio',
+            'aiohttp']
+setup(
+    name='deuce',
+    version='0.2',
+    description='Deuce - Block-level de-duplication as-a-service',
+    license='Apache License 2.0',
+    url='github.com/rackerlabs/deuce',
+    author='Rackspace',
+    author_email='',
+    install_requires=REQUIRES,
+    test_suite='deuce',
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'deuce-server = deuce.cmd.server:run',
+        ]
+    },
+    data_files=[('config', ['ini/config.ini', 'ini/configspec.ini'])],
+    # include_package_data=True,
+    packages=find_packages(exclude=['tests*', 'deuce/tests*'])
+    # package_data={'deuce': ['ini/*.ini']}
+)
