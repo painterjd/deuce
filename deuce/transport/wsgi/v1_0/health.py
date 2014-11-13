9675a45707697e9bf958574fa7e89b4a0fad5024
BenjamenMeyer <ben.meyer@rackspace.com>
2014-11-13 14:36:39 +0000

    Cleanup - imorts
    In trying to test Falcon master to fix the issue with no body in the 405 reponse I discovered we had an import that Falcon removed (falcon.api_helpers) in latest master.
    Only three files imported it, and none needed it. cleaned up the imports so that these three are using their minimal imports. We should now be compatible with Falcon master branch too.

--- deuce/transport/wsgi/v1_0/health.py
+++ deuce/transport/wsgi/v1_0/health.py
@@ -1,17 +1,10 @@
-import falcon
-from falcon import api_helpers
-import msgpack
-import six
 import json
-from six.moves.urllib.parse import urlparse, parse_qs
-from deuce.util import set_qs
-from deuce.model import Vault
+
+import falcon
 
 import deuce.util.log as logging
-from deuce.transport.validation import *
-from deuce.util.filecat import FileCat
-import deuce
 from deuce.model import Health
+
 
 logger = logging.getLogger(__name__)
 
