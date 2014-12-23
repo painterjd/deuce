b2cc752b9b9b74a10517d0ec4c99f0fa391a9a61
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2014-12-23 13:11:30 -0500

    rm: redudancy in mock cassandra's execute_async

--- deuce/tests/mock_cassandra/__init__.py
+++ deuce/tests/mock_cassandra/__init__.py
@@ -89,71 +89,7 @@ class Session(object):
         return res
 
     def execute_async(self, query, queryargs):
-        # Health check.
-        if 'system.local' in query:
-            return 'true'
 
-        original_query = query
-
-        query = query.replace('false', '0')
-        query = query.replace('true', '1')
-
-        if isinstance(queryargs, tuple):
-
-            # convert UUID to string
-            queryargs = tuple([str(s) if isinstance(s, uuid.UUID)
-                           else s for s in queryargs])
-
-            # sqlite prefers ? over %s for positional args
-            query = query.replace('%s', '?')
-
-        elif isinstance(queryargs, dict):
-
-            # If the user passed dictionary arguments, assume that they
-            # used that cassandra %(fieldname)s and convert to sqlite's
-            # :fieldname
-
-            for k, v in queryargs.items():
-                cass_style_arg = "%({0})s".format(k)
-                sqlite_style_arg = ":{0}".format(k)
-                query = query.replace(cass_style_arg, sqlite_style_arg)
-
-                # Convert UUID parameters to strings
-                if isinstance(v, uuid.UUID):
-                    queryargs[k] = str(v)
-
-        if original_query == actual_driver.CQL_INC_BLOCK_REF_COUNT:
-
-            # Special-case this query, since sqlite doesn't
-            # support upserts
-
-            insert_query = """
-                INSERT or IGNORE into blockreferences
-                (projectid, vaultid, blockid, refcount)
-                VALUES
-                (:projectid, :vaultid, :blockid, :refcount)
-            """
-
-            insert_args = queryargs.copy()
-            insert_args.update({'refcount': 0})
-            del insert_args["delta"]
-
-            self.conn.execute(insert_query, insert_args)
-
-        elif original_query == actual_driver.CQL_UPDATE_REF_TIME or \
-                original_query == actual_driver.CQL_REGISTER_BLOCK:
-
-            # unixTimeStampOf() and now() are not part of SQLite
-            query = query.replace('unixTimeStampOf(now())',
-                                  "strftime('%s', 'now')")
-
-        elif original_query == actual_driver.CQL_HEALTH_CHECK:
-
-            # neither now() nor system.local are part of sqlite
-            # So the following gives us the same query as the original
-            query = "SELECT strftime('%s', 'now')"
-
-        res = self.conn.execute(query, queryargs)
-        # res = list(res)
+        res = self.execute(query, queryargs)
 
         return Future(res)
