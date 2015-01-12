b47dbd2bf34b5900d7a0e97765ae1bfedb3cbe76
BenjamenMeyer <ben.meyer@rackspace.com>
2015-01-12 22:22:45 +0000

    Enhancement: Provide the ability to track a block to storage for the msgpack upload
    - msgpack upload (POST) functionality was not reporting the storage block ids while reporting a 201
      -- updated to a 200 response
      -- updated for the body to contain a JSON-encoded dict mapping the block id to the storage block id
      -- updated unit tests as appropriate
      -- added functionality to the success unit test to verify that the storage id provided in the POST response would match what was returned in the GET response's header for the given block

--- deuce/model/vault.py
+++ deuce/model/vault.py
@@ -93,6 +93,8 @@ class Vault(object):
             block_ids,
             blockdatas)
 
+        retblocks = []
+
         # (BenjamenMeyer): If we fail to upload any one block then we
         # let the Validation and Clean-Up Service remove any uploaded blocks
         # and fail out the request as a whole
@@ -118,8 +120,9 @@ class Vault(object):
                     block_id,
                     storageid,
                     block_size)
+                retblocks.append((block_id, storageid))
 
-        return retval
+        return (retval, retblocks)
 
     def get_blocks(self, marker, limit):
         gen = deuce.metadata_driver.create_block_generator(
